179113b94283b429982807e8ee5736c1d9d7adc4
Mukhammadziyo <244293343+mukhammadziyo-prog@users.noreply.github.com>
2026-08-13 23:47:26 +0500

    Methods

--- first_program.java
+++ first_program.java
@@ -17,3 +17,10 @@ Note: Java is case-sensitive. MyClass and myclass would be treated as two comple
 The name of the Java file must match the class name. So if your class is called Main, the file must be saved as Main.java.
 
 This is because Java uses the class name to find and run your code. If the names don't match, Java will give an error and the program will not run.
+
+------------------------------------------------------------------
+
+The main Method
+The main() method is required in every Java program. It is where the program starts running:
+
+public static void main(String[] args)
