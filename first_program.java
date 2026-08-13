319be946406fc65cee68fb0004a7c8b3befc4cbe
Mukhammadziyo <244293343+mukhammadziyo-prog@users.noreply.github.com>
2026-08-13 23:27:29 +0500

    example

--- first_program.java
+++ first_program.java
@@ -6,4 +6,14 @@ class Main {
 
 
 Waiting for update coming soon...
-Still waiting for it...
+
+
+-----------------------------------------------------------------
+
+Example explained
+
+Every line of code that runs in Java must be inside a class. The class name should always start with an uppercase first letter. In our example, we named the class Main.
+
+Note: Java is case-sensitive. MyClass and myclass would be treated as two completely different names.
+
+The name of the Java file must match the class name. So if your class is called Main, the file must be saved as Main.java. This is because Java uses the class name to find and run your code. If the names don't match, Java will give an error and the program will not run.
